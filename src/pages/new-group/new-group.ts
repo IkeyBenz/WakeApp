158c1c417b8655d829849ec028e602db7c154cba
Ikey Benzaken <ikey.benz@gmail.com>
2018-10-16 00:54:29 -0700

    Added ui for joining a group now too

--- src/pages/new-group/new-group.ts
+++ src/pages/new-group/new-group.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the NewGroupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-new-group',
@@ -15,11 +8,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class NewGroupPage {
 
+  creating = true;
+  accessCode = "";
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewGroupPage');
+  }
+  toggleCreating() {
+    if (this.creating) { this.creating = false }
+    else { this.creating = true }
+  }
+  joinGroup() {
+
+  }
+  createGroup() {
+    
   }
 
 }
